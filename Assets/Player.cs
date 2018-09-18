74bd1a1364ee59d5fee1dd85983e82565924dfb8
Kartik Gupta <kartikg1997@gmail.com>
2018-09-18 14:26:41 +0530

    added bounce functionality to ball
    if ball goes in +x, keep moving in +x. if not, move in -x. When the ball hits the wall, it bounces in opposite direction. So, the direction changes because of hitting the wall
    Added variable JumpX and JumpY to control the bounce. Also added trail to the ball

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    Rigidbody2D rb;
+
+    int JumpX = 3;
+    int JumpY = 9;
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        
+
+        if (Input.GetMouseButtonDown(0)) {
+            //if ball goes in +x, keep moving in +x. if not, move in -x. 
+            //When the ball hits the wall, it bounces in opposite direction. 
+            //So, the direction changes because of hitting the wall
+            if (rb.velocity.x >= 0)
+            {
+                rb.velocity = new Vector2(JumpX, JumpY);
+            }
+            else {
+
+                rb.velocity = new Vector2(-JumpX, JumpY);
+            }
+        }
 	}
 }
