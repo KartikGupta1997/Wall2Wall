5359ab36e221abd76e7136d9b5e5789bf349c693
Kartik Gupta <kartikg1997@gmail.com>
2018-09-20 16:53:30 +0530

    Added obstacle
    Added randomisation of obstacle everytime we start the game

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour {
+
+    public GameObject triangleObj;
+	// Use this for initialization
+	void Start () {
+
+        initObstacles();
+        	
+	}
+
+    void initObstacles() {
+        int i;
+        for (i=0; i<12; i++)
+        {
+            int randomY = Random.Range(-6, 7); 
+            GameObject tempObj = Instantiate(triangleObj, new Vector2(transform.position.x, randomY), Quaternion.identity);
+            //all the instantiated gameobjects will become the child of the wall, using the line of code below
+            tempObj.transform.SetParent(transform);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
