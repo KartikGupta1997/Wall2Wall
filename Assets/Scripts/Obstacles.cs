d991529e8d9a2eaa2b6ffd926d972feae56a742e
Kartik Gupta <kartikg1997@gmail.com>
2018-09-22 00:41:35 +0530

    recommit previous changes completely

--- Assets/Scripts/Obstacles.cs
+++ Assets/Scripts/Obstacles.cs
@@ -13,30 +13,23 @@ public class Obstacles : MonoBehaviour {
 	}
 
     void initObstacles() {
-
-        //this foreach loop will destroy all the obstacles from the previous loop for loop (destroys obstacles from the start function in first iteration)
-        foreach (Transform child in transform) {
-            GameObject.Destroy(child.gameObject);
-        }
-        for (int i=0; i<12; i++)
+        int i;
+        for (i=0; i<12; i++)
         {
             int randomY = Random.Range(-6, 7); 
+<<<<<<< HEAD
 
             //randomly selected numbers from the range will instantiate an obstacle there
+=======
+>>>>>>> parent of 054d05a... added autospawn of obstacles
             GameObject tempObj = Instantiate(triangleObj, new Vector2(transform.position.x, randomY), Quaternion.identity);
             
             //all the instantiated gameobjects will become the child of the wall, using the line of code below
             tempObj.transform.SetParent(transform);
         }
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Name:" + collision.gameObject.name);
-        initObstacles();
-    }
-    // Update is called once per frame
-    void Update () {
+	// Update is called once per frame
+	void Update () {
 		
 	}
 }
